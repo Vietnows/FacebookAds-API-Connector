5e76ede2f04132fb5614df4cd73c63defed9a854
Marcelo Oliveira <marcello.henrique1994@gmail.com>
2019-08-28 13:26:10 -0300

    Update DataNormalize

--- FacebookNormalizeData.py
+++ FacebookNormalizeData.py
@@ -9,7 +9,6 @@ from bson.json_util import dumps
 import json
 import pandas as pd
 
-
 ''' 
 Pay attention!!!
 
@@ -25,16 +24,40 @@ def dumps_data(data):
     
     return dataNormalize
 
+def rename_columns(dataNormalize):
+    dataNormalize.rename(columns={"ad_name":"Ad Name",
+                                         "adset_name":"Ad Set Name",
+                                         "campaign_name":"Campaign Name",
+                                         "spend":"Amount Spent (BRL)",
+                                         "frequency":"Frequency",
+                                         "impressions":"Impressions",
+                                         "inline_link_clicks":"Link Clicks",
+                                         "reach":"Reach",
+                                         "date_start":"Reporting Starts",
+                                         "date_stop":"Reporting Ends"},inplace=True)
+    return dataNormalize
+
 '''
 Below I change the types of the columns.
 '''
 def change_types(dataNormalize):
     dataNormalize['spend'] = dataNormalize['spend'].apply(float)
-    dataNormalize['cpm'] = dataNormalize['cpm'].apply(float)
-    dataNormalize['inline_link_click_ctr'] = dataNormalize['inline_link_click_ctr'].apply(float)
+    #dataNormalize['cpm'] = dataNormalize['cpm'].apply(float)
+    #dataNormalize['inline_link_click_ctr'] = dataNormalize['inline_link_click_ctr'].apply(float)
     dataNormalize['impressions'] = dataNormalize['impressions'].apply(int)
     dataNormalize['inline_link_clicks'] = dataNormalize['inline_link_clicks'].apply(int)
     dataNormalize['reach'] = dataNormalize['reach'].apply(int)
+    dataNormalize['frequency'] = dataNormalize['frequency'].apply(float).round(2)
+    dataNormalize['date_start'] = pd.to_datetime(dataNormalize['date_start'])
+    dataNormalize['date_stop'] = pd.to_datetime(dataNormalize['date_stop'])
+    
     dataNormalize = dataNormalize.fillna(0)
     
     return dataNormalize
+
+def reorder_columns(dataNormalize):
+    return dataNormalize[['ad_name','adset_name','campaign_name','spend',
+                          'frequency','impressions','inline_link_clicks',
+                          'reach','date_start','date_stop']]
+
+
